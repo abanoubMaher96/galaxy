414faf4121ab5c84522a00853ac502193e1bb8b4
unknown <abanoubmaher2015@gmail.com>
2019-10-12 22:01:42 +0200

    Updates

---
+++ precache-manifest.8df06779c5f4788810d3c5d694ef8196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b362b7cd065b7485ed29a073f98cf2",
+    "url": "/galaxy/index.html"
+  },
+  {
+    "revision": "624c60a48ae56e958fa2",
+    "url": "/galaxy/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "13b90725fa007e86d0af",
+    "url": "/galaxy/static/css/main.3b1ccf90.chunk.css"
+  },
+  {
+    "revision": "624c60a48ae56e958fa2",
+    "url": "/galaxy/static/js/2.7f6676e7.chunk.js"
+  },
+  {
+    "revision": "13b90725fa007e86d0af",
+    "url": "/galaxy/static/js/main.2973d5a0.chunk.js"
+  },
+  {
+    "revision": "c57643e7fc60696fae8c",
+    "url": "/galaxy/static/js/runtime-main.5546cdc7.js"
+  }
+]);
